fac57fd486adda780f40045ad47088e2449482fa
Davide Natale <s318967@studenti.polito.it>
2024-12-12 17:21:37 +0100

    feat: font and icons configurations added

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,54 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={[ styles.interBlack, { fontSize: 15 }]}>Example text for font family</Text>
+      <Text style={[ styles.interExtraBold, { fontSize: 15 }]}>Example text for font family</Text>
+      <Text style={[ styles.interBold, { fontSize: 15 }]}>Example text for font family</Text>
+      <Text style={[ styles.interSemiBold, { fontSize: 15 }]}>Example text for font family</Text>
+      <Text style={[ styles.interMedium, { fontSize: 15 }]}>Example text for font family</Text>
+      <Text style={[ styles.interRegular, { fontSize: 15 }]}>Example text for font family</Text>
+      <Text style={[ styles.interLight, { fontSize: 15 }]}>Example text for font family</Text>
+      <Text style={[ styles.interExtraLight, { fontSize: 15 }]}>Example text for font family</Text>
+      <Text style={[ styles.interThin, { fontSize: 15 }]}>Example text for font family</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  interBlack: {
+    fontFamily: "inter_black",
+    color: "blue"
+  },
+  interExtraBold: {
+    fontFamily: "inter_extrabold",
+    color: "blue"
+  },
+  interBold: {
+    fontFamily: "inter_bold",
+    color: "blue"
+  },
+  interSemiBold: {
+    fontFamily: "inter_semibold",
+    color: "blue"
+  },
+  interMedium: {
+    fontFamily: "inter_medium",
+    color: "blue"
+  },
+  interRegular: {
+    fontFamily: "inter_regular",
+    color: "blue"
+  },
+  interLight: {
+    fontFamily: "inter_light",
+    color: "blue"
+  },
+  interExtraLight: {
+    fontFamily: "inter_extralight",
+    color: "blue"
+  },
+  interThin: {
+    fontFamily: "inter_thin",
+    color: "blue"
+  },
+});
