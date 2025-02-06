528b8122da4d55ce90288099cd0ea9b770bbc6d9
Davide Natale <s318967@studenti.polito.it>
2025-02-06 15:43:56 +0100

    feat: dependency configuration to use push notification added feat: reconnection socket.io code implementation added

--- utils/webSocket.ts
+++ utils/webSocket.ts
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
 import { getToken } from './secureStore';
+import authAPI from '@/api/authAPI';
 
 const SERVER_URL = 'http://192.168.178.183:3000';
 
@@ -11,20 +12,54 @@ const socket = io(SERVER_URL, {
     transports: ['websocket']
 });
 
+const checkAuth = async () => {
+    try {
+        //  Verify authentication to force token refresh if needed
+        await authAPI.verifyAuth();
+
+        //  Retrieve new access token fron Secure Store
+        const newAccessToken = await getToken('accessToken');
+
+        return newAccessToken; 
+    } catch (error) {
+        return null;
+    }
+}
+
 export const connectSocket = async () => {
     const accessToken = await getToken('accessToken');
 
-    if (accessToken) {
+    if(accessToken) {
         socket.auth = { token: `Bearer ${accessToken}` };
-
         socket.connect();
 
-        socket.on('connect', () => {
-            console.log('Connected to Socket: ', socket.connected);
+        socket.on('reconnect_attempt', async () => {
+            const latestAccessToken = await getToken('accessToken');
+            if(latestAccessToken) socket.auth = { token: `Bearer ${latestAccessToken}` };
         });
 
-        socket.on('disconnect', () => {
-            console.log("Disconnected from Socket: ", socket.disconnected);
+        socket.on('reconnect_error', async (error) => {
+            if(error.message === 'Access Token missing.' ||
+               error.message === 'Access Token blacklisted.' ||
+               error.message === 'Invalid Access Token.'
+            ) {
+                const newAccessToken = await checkAuth();
+                if(newAccessToken) socket.auth = { token: `Bearer ${newAccessToken}` };
+            } 
+        });
+
+        socket.on('connect_error', async (error) => {
+            if(error.message === 'Access Token missing.' ||
+                error.message === 'Access Token blacklisted.' ||
+                error.message === 'Invalid Access Token.'
+             ) {
+                const newAccessToken = await checkAuth();
+                
+                if (newAccessToken) {
+                    socket.auth = { token: `Bearer ${newAccessToken}` };
+                    socket.connect();
+                }
+             } 
         });
     }
 };
